5c06c160273e19b08182cc97a2e363f2b54a1b15
whyuan00 <whyuan00@gmail.com>
2025-01-06 19:13:15 +0900

    chore: 프로젝트 초기 설정

---
+++ be/src/main/java/com/example/dojusik/DojusikApplication.java
@@ -0,0 +1,13 @@
+package com.example.dojusik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojusikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojusikApplication.class, args);
+	}
+
+}
