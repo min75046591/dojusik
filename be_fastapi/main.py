11d58cdfa76b873b2e04ecbf0088be8461b3d3f6
shyeon <kevinyeonkr@naver.com>
2025-01-19 23:24:24 +0900

    feat: fastapi and websocket

---
+++ be_fastapi/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, APIRouter
+from router.stock import stock_router
+from fastapi.staticfiles import StaticFiles
+
+
+app = FastAPI()
+routers = APIRouter()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the FastAPI app!"}
+
+app.include_router(router=stock_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
